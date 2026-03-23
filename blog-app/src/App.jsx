be403b519883d6e7114721049d32bce37672acde
ee17btech11034 <asiwalraja@gmail.com>
2026-03-23 14:16:12 +0530

    configure redux toolkit

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,16 +1,34 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import {login, logout} from "./store/authSlice"
 
 function App() {
-  const new_var1 = import.meta.env.VITE_APPWRITE_URL;
-  const new_var2 = import.meta.env.VITE_APPWRITE_URL2;
-  console.log(new_var1);
-  
+  const [loading, setLoading] = useState(true); // are we  fetching the datat from appwrite
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    authService.isuserLoggedIn()
+          .then((data)=>{ // datat will be 
+            console.log("is user logged in: ", data)
+            if (data){
+              dispatch(login({userData: data}))
+            }
+            else{
+              dispatch(logout()) // we are calling logout so that our app's state is updated on all tasks.
+            }
+          })
+          .finally(()=> setLoading(false)) // as loading is completed
+  }, [])
 
-  return (
-    <>
-      <div>Hi Raj, {new_var1}, {new_var2}</div>
-    </>
-  )
+
+  return ((loading) ? (<div>Hi Raj, still loading the data</div>) : (<div>Hi Raj, data is loaded</div>))
+
+  /*
+  <Header></Header>
+  <main> {outlet}</main>    --> we will handle this in this way
+  <Footer></Footer>
+
+  */
 }
 
 export default App
