f584fea7f241ed8b8619fd2141124e380126117e
ee17btech11034 <asiwalraja@gmail.com>
2026-03-24 18:38:40 +0530

    Pages and router

--- blog-app/src/main.jsx
+++ blog-app/src/main.jsx
@@ -3,11 +3,42 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>
+      },
+      
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}> // no authentication need for Login access
+            <login/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/all-posts',
+        element: (
+          <AuthLayout authentication={true}>
+            <login/>
+          </AuthLayout>
+        )
+      },
+
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 )
